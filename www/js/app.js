e71d6ef3fa3e83508fe28f791b27a5f048e721ae
wilsonhobbs <wilsonhobbs1@gmail.com>
2015-09-20 18:56:44 -0400

    removed comments.services

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('comments', ['ionic', "firebase", "comments.controllers", "comments.services"])
+angular.module('comments', ['ionic', "firebase", "comments.controllers"])
 	.config(function($urlRouterProvider, $stateProvider) {
 		$stateProvider.state("posts", {
 				url: "/posts",
